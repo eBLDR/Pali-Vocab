f714d3c115538b555af188945cf3e2421fa88921
eBLDR <ed.bldr@gmail.com>
2020-11-04 10:09:02 +0100

    Refactored folder structure

--- palivocab/config.py
+++ palivocab/config.py
@@ -1,10 +1,8 @@
 import os
 
-ALL_STRING = 'all'
-
-CSV_EXTENSION = '.csv'
-
 PROJECT_NAME = 'Pali Vocabulari'
-SRC_PATH = os.path.join(os.getcwd(), 'palivocab', 'src')
-
+SRC_PATH = os.path.join(os.getcwd(), 'data')
 LESSON_FOLDER_NAME = 'lesson{two_digit_number}'
+
+ALL_STRING = 'all'
+CSV_EXTENSION = '.csv'
